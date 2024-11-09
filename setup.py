c657dbecf1c3daae01a61a9ce7cec947013fb871
Mario Morales <mario@moralesalfaro.cl>
2024-11-08 16:51:29 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="dynprog",
+    version="0.1.0",
+    description="Dynammic programming",
+    long_description=readme,
+    author="Mario Morales",
+    author_email="mario@moralesalfaro.cl",
+    url="https://github.com/marioles/dynprog",
+    packages=find_packages(exclude=("tests", "docs"))
+)
